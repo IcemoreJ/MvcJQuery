1ba97601adda8ee36cfc0e05baa4257285f892ef
IcemoreJ <864510745@qq.com>
2018-02-09 11:30:46 +0800

    添加项目文件。

---
+++ MvcJQuery/Controllers/HomeController.cs
@@ -0,0 +1,107 @@
+﻿using MvcJQuery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcJQuery.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        MyFirstDatabaseEntities db = new MyFirstDatabaseEntities();
+        public ActionResult Index()
+        {
+            List<UserInfos> list = db.UserInfos.ToList();
+            return View(list);
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(UserInfo user)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        userServer.Add(user);
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View();
+        //}
+
+        //public ActionResult Details(int id = 0)
+        //{
+        //    var user = userServer.GetById(id);
+        //    if (user == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(user);
+        //}
+
+        //public ActionResult Delete(int id = 0)
+        //{
+        //    var user = userServer.GetById(id);
+        //    if (user == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+
+        //    return View();
+        //}/
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteInfo(int id)
+        //{
+        //    var user = userServer.GetById(id);
+
+        //    userServer.Delete(user);
+        //    return RedirectToAction("Index");
+        //}
+
+        //public ActionResult Edit(int id = 0)
+        //{
+        //    var user = userServer.GetById(id);
+
+        //    if (user == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+
+        //    return View(user);
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(UserInfo user)
+        //{
+        //    bool flag = userServer.Updata(user);
+
+        //    if (flag == false)
+        //    {
+        //        return HttpNotFound();
+        //    }
+
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
---
+++ MvcJQuery/Scripts/Method.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+   // $("button").click(function () {
+   // $("button").dblclick(function () {
+    $("button").click(function () {
+        $(this).hide();
+    });
+    $("#btn1").dblclick(function () {
+        $(this).hide();
+    });
+});
